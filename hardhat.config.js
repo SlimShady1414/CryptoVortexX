d1feb9a34efae232a19248ae4dc76b26ec622c87
AdhiNandanJ <adhinandanj@gmail.com>
2024-01-12 21:43:29 +0530

    initial npm and hardhat configs

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config(); //allows us go use env varibales in hardhat project.
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+ 
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+module.exports = {
+solidity: "0.8.4",
+};
