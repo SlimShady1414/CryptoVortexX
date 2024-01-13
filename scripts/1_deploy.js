fc5ece31b7e0f0c0534dd9b34b9ef595022d2674
AdhiNandanJ <adhinandanj@gmail.com>
2024-01-13 12:10:52 +0530

    testing contracts

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,17 +1,16 @@
 async function main() {
-  const Token = await ethers.getContractFactory("Token") //ether is a library, gCF gets info from artifact folders to use smart contracts in blockchain
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
 
-  //deploying the contracct
+  // Deploy contract
   const token = await Token.deploy()
   await token.deployed()
   console.log(`Token Deployed to: ${token.address}`)
-
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((error) =>{
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
-
