f6465510d84f5755ea6980a85c6366e891f5ef00
AdhiNandanJ <adhinandanj@gmail.com>
2024-01-13 14:57:09 +0530

    track balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,14 +4,22 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
+	//storing infos
     string public name;
     string public symbol;
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    //track balances
+    mapping(address => uint256) public balanceOf; //mapping is a data structure(key-value pair)
+    //send tokens
+
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply; //msg.sender is an address
+
+
     }
 }
