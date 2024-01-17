c94f82491910d82a7cc3c5fee63843ee0c4a3bae
AdhiNandanJ <adhinandanj@gmail.com>
2024-01-17 18:44:55 +0530

    final seeding before frontend

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -18,9 +18,11 @@ contract Token {
         uint256 value
     );
 
-    event Approval(address indexed owner,  address indexed spender, uint256 value);
-
-
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
     constructor(
         string memory _name,
@@ -38,36 +40,53 @@ contract Token {
         returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value);
+
         _transfer(msg.sender, _to, _value);
 
         return true;
     }
 
-    function _transfer(address _from, address _to, uint256 _value) internal {
-    	require(_to != address(0));
-    	balanceOf[_from] = balanceOf[_from] - _value;
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
+
+        balanceOf[_from] = balanceOf[_from] - _value;
         balanceOf[_to] = balanceOf[_to] + _value;
+
         emit Transfer(_from, _to, _value);
     }
 
+    function approve(address _spender, uint256 _value)
+        public
+        returns(bool success)
+    {
+        require(_spender != address(0));
 
-    function approve(address _spender, uint256 _value) public returns(bool success){
-    	require(_spender != address(0));
-    	allowance[msg.sender][_spender] = _value;
+        allowance[msg.sender][_spender] = _value;
 
-    	emit Approval(msg.sender, _spender, _value);
-    	return true;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
-    	require(_value <= balanceOf[_from]);
-    	require(_value <= allowance[_from][msg.sender]);
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    )
+        public
+        returns (bool success)
+    {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
 
-    	allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
 
-    	_transfer(_from, _to, _value);
-    	return true;
+        _transfer(_from, _to, _value);
+
+        return true;
     }
-
 
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config(); //allows us go use env varibales in hardhat project.
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,13 +10,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-solidity: "0.8.9",
-networks: {
-  localhost: {}
-
-},
+  solidity: "0.8.9",
+  networks: {
+    localhost: {}
+  },
 };
